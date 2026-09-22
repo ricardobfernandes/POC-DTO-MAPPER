0ccc71f04108a75a672c6f045803a16f5e2b9c1a
Ricardo Fernandes <ricardo.b.fernandes@accenture.com>
2026-09-22 17:47:59 -0300

    First Commit

---
+++ src/main/java/com/ricardo/POCdtomapper/PoCdtomapperApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.POCdtomapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCdtomapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCdtomapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/POCdtomapper/PoCdtomapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.POCdtomapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCdtomapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
